93b7a6b2f2b08617690b5953367c1bb618bca602
Jeroen Seeverens <j.seeverens@xentropics.nl>
2023-12-07 22:50:10 +0100

    Updated install-modules

--- xentropics:omeka-s-docker/files/scripts/install-modules.sh
+++ xentropics:omeka-s-docker/files/scripts/install-modules.sh
@@ -20,26 +20,30 @@ else
 fi
 
 for (( i=0; i<$MODULE_COUNT; i++ ))
-    do
-
+    do     
         echo "install-modules.sh : reading module information"
-        MODULE_NAME=$(eval "jq -r '.modules[${i}]'.name /opt/imageboot/profile.json")
-        MODULE_VERSION=$(eval "jq -r '.modules[${i}]'.version /opt/imageboot/profile.json")
-        MODULE_URL_TEMPLATE=$(eval "jq -r '.modules[${i}]'.zipUrl /opt/imageboot/profile.json")
-        MODULE_URL=${MODULE_URL_TEMPLATE//'${version}'/"$MODULE_VERSION"}
-        MODULE_ENABLE=$(eval "jq -r '.modules[${i}]'.install /opt/imageboot/profile.json")
+        MODULE_INSTALL=$(eval "jq -r '.modules[${i}]'.install /opt/imageboot/profile.json")
 
-        echo "install-modules.sh : fetching $MODULE_NAME"
-        curl -s -L -o /tmp/$MODULE_NAME.zip $MODULE_URL
+        if $MODULE_INSTALL
+        then
+                MODULE_VERSION=$(eval "jq -r '.modules[${i}]'.version /opt/imageboot/profile.json")
+                MODULE_NAME_TEMPLATE=$(eval "jq -r '.modules[${i}]'.name /opt/imageboot/profile.json")
+                MODULE_NAME=${MODULE_NAME_TEMPLATE//'${version}'/"$MODULE_VERSION"}
+                MODULE_URL_TEMPLATE=$(eval "jq -r '.modules[${i}]'.zipUrl /opt/imageboot/profile.json")
+                MODULE_URL=${MODULE_URL_TEMPLATE//'${version}'/"$MODULE_VERSION"}
+                MODULE_ENABLE=$(eval "jq -r '.modules[${i}]'.install /opt/imageboot/profile.json")
+
+                echo "install-modules.sh : fetching $MODULE_NAME"
+                curl -s -L -o /tmp/$MODULE_NAME.zip $MODULE_URL
     
-        echo "install-modules.sh : unzipping $MODULE_NAME.zip"
-        unzip -d /tmp/ /tmp/$MODULE_NAME.zip && mv /tmp/$MODULE_NAME /var/www/html/modules/$MODULE_NAME && rm -rf /tmp/$MODULE_NAME*
+                echo "install-modules.sh : unzipping $MODULE_NAME.zip"
+                unzip -d /tmp/omeka/ /tmp/$MODULE_NAME.zip && mv /tmp/omeka/**/ /var/www/html/modules/$MODULE_NAME && rm -rf /tmp/$MODULE_NAME* && rm -rf /tmp/omeka
 
-        MODULE_PAGE=$(curl -s -b "cookie.jar" -c "cookie.jar" -L http://localhost:80/admin/module | tidy -quiet -asxml) 
-        CSRF_TOKEN=$(echo $MODULE_PAGE | xmlstarlet sel -t -v "//_:form[contains(@action,'install?id=$MODULE_NAME')]/_:input[@name='csrf']/@value")
-        ACTION=$(echo $MODULE_PAGE | xmlstarlet sel -t -v "//_:form[contains(@action,'install?id=$MODULE_NAME')]/@action")
-        echo $CSRF_TOKEN
-        echo $ACTION
-        curl -L -s -b "cookie.jar" -c "cookie.jar" -X POST -H "X_CSRF-Token:${CSRF_TOKEN}" -F "csrf=${CSRF_TOKEN}" -F "id=" http://localhost:80${ACTION} | tidy -quiet -asxml | xmlstarlet sel -t -v "//_:ul[@class='messages']"
-        
+                MODULE_PAGE=$(curl -s -b "cookie.jar" -c "cookie.jar" -L http://localhost:80/admin/module | tidy -quiet -asxml) 
+                CSRF_TOKEN=$(echo $MODULE_PAGE | xmlstarlet sel -t -v "//_:form[contains(@action,'install?id=$MODULE_NAME')]/_:input[@name='csrf']/@value")
+                ACTION=$(echo $MODULE_PAGE | xmlstarlet sel -t -v "//_:form[contains(@action,'install?id=$MODULE_NAME')]/@action")
+                echo $CSRF_TOKEN
+                echo $ACTION
+                curl -L -s -b "cookie.jar" -c "cookie.jar" -X POST -H "X_CSRF-Token:${CSRF_TOKEN}" -F "csrf=${CSRF_TOKEN}" -F "id=" http://localhost:80${ACTION} | tidy -quiet -asxml | xmlstarlet sel -t -v "//_:ul[@class='messages']"
+        fi
     done
