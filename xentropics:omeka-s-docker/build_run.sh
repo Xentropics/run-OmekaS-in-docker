c5b60a2fda14495f4ec1e97ed757fc191b06a965
Jeroen Seeverens <j.seeverens@xentropics.nl>
2023-12-21 21:36:32 +0100

    add parameters to build_run script

--- xentropics:omeka-s-docker/build_run.sh
+++ xentropics:omeka-s-docker/build_run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-docker build . --tag=xentropics/omeka-s:0.3.0
+par_image_tag="0.4.0"
+while getopts v:c:t: flag
+do
+    case "${flag}" in
+        v) par_omeka_s_version="--build-arg=omeka_version=${OPTARG}";;
+        c) par_config_profile="--build-arg=config_profile=${OPTARG}";;
+        t) par_image_tag="${OPTARG}";;
+        u) echo "Usage: $0 [-v omeka_version] [-t image_tag] [-c config_profile]"; exit 1;;
+    esac
+done
+docker build . --tag=xentropics/omeka-s:$par_image_tag $par_omeka_s_version $par_config_profile
 docker container prune -f
+export tag=$par_image_tag
 docker-compose up
