8ce105e9aaf9038a1dd0e9c1a79f6f5bfe6fb74c
Jeroen Seeverens <j.seeverens@xentropics.nl>
2024-01-02 21:59:26 +0100

    trying some stuff

--- xentropics:omeka-s-docker/build_run.sh
+++ xentropics:omeka-s-docker/build_run.sh
@@ -9,7 +9,7 @@ do
         u) echo "Usage: $0 [-v omeka_version] [-t image_tag] [-c config_profile]"; exit 1;;
     esac
 done
-docker build . --tag=xentropics/omeka-s:$par_image_tag $par_omeka_s_version $par_config_profile
+docker build . --progress=plain --tag=xentropics/omeka-s:$par_image_tag $par_omeka_s_version $par_config_profile
 docker container prune -f
 export tag=$par_image_tag
 docker-compose up
--- xentropics:omeka-s-docker/files/scripts/install-ark-deps.sh
+++ xentropics:omeka-s-docker/files/scripts/install-ark-deps.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "install-ark-deps.sh : installing ark deps"
 
-if [[ "$install_arkdeps" == "yes" ]]
+if [[ "$OMEKA_INSTALL_ARKDEPS" == "yes" ]]
 then
     echo "install-ark-deps.sh : installing ark deps"
 else
     echo "install-ark-deps.sh : skipping ark deps"
     exit 0
 fi
-apt-get update && apt-get install -y $berkeleydb_lib
+apt-get update && apt-get install -y $OMEKA_BERKELEYDB_LIB
 ln -s /usr/include /opt/include
 ln -s /usr/lib/aarch64-linux-gnu/ /opt/lib
 docker-php-ext-configure dba --with-db4=/opt
