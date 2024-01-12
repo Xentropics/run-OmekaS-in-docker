a07c1abc6872a4ce47c6fcc9655bdbf5aacbf958
Jeroen Seeverens <j.seeverens@xentropics.nl>
2024-01-13 00:30:56 +0100

    even less state... and folder structure overhaul

--- xentropics:omeka-s-docker/includes/getopts.sh
+++ xentropics:omeka-s-docker/includes/getopts.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 while getopts "v:c:t:mu" flag
 do
     case "${flag}" in
         v) par_omeka_s_version="--build-arg=omeka_version=${OPTARG}";;
-        c) par_config_profile="--build-arg=config_profile=${OPTARG}" && config_profile="${OPTARG}";;
+        c) config_profile="${OPTARG}";;
         t) par_image_tag="${OPTARG}";;
         m) skip_modules="no";;
         u|\?) echo "Usage: $0 [-v omeka_version] [-c config_profile] [-t image_tag] [-m] [-u]"; exit 1;;
     esac
 done
-echo $skip_modules
-export tag=$par_image_tag
+
+export OMEKA_IMAGE_TAG=$par_image_tag
 export OMEKA_SKIP_MODULES=$skip_modules
 export OMEKA_PROFILE=$config_profile
