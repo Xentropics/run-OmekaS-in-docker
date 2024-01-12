4c4eb5c21a521408f923300d2c5578d3ee462dd8
Jeroen Seeverens <j.seeverens@xentropics.nl>
2024-01-12 22:46:43 +0100

    added profiles to folders and refactored references to profiles

--- xentropics:omeka-s-docker/run.sh
+++ xentropics:omeka-s-docker/run.sh
@@ -2,5 +2,5 @@
 source ./includes/defaults.sh
 source ./includes/getopts.sh
 docker container prune -f
-docker-compose --project-name=$config_profile --env-file=./files/etc/profiles/$config_profile.env up 
+docker-compose --project-name=$config_profile --env-file=./files/etc/profiles/$config_profile/.env up 
 docker container prune -f
