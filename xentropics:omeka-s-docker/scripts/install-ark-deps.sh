0552ed8048da2418885962e8a0340f66cd4005a3
Jeroen Seeverens <j.seeverens@xentropics.nl>
2024-02-07 13:05:51 +0100

    another shot

--- xentropics:omeka-s-docker/scripts/install-ark-deps.sh
+++ xentropics:omeka-s-docker/scripts/install-ark-deps.sh
@@ -10,12 +10,7 @@ apt-get update && apt-get install -y $berkeleydb_lib
 arch=$(uname -m)
 echo "Architecture : $arch"
 ln -s -f /usr/include /opt/include
-if [[ $arch == "aarch64" ]]
-then
-    ln -s -f /usr/lib/aarch64-linux-gnu/ /opt/lib
-else
-    ln -s -f /usr/lib64/ /opt/lib
-fi
+ln -s -f /usr/lib/${arch}-linux-gnu/ /opt/lib
 
 docker-php-ext-configure dba --with-db4=/opt
 docker-php-ext-install dba
