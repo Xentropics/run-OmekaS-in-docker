2edc4194e346a6c019d16ae01fa840d253f035c8
Jeroen Seeverens <j.seeverens@xentropics.nl>
2024-02-07 13:20:52 +0100

    better

--- xentropics:omeka-s-docker/run.sh
+++ xentropics:omeka-s-docker/run.sh
@@ -3,5 +3,5 @@ source ./includes/defaults.sh
 source ./includes/getopts.sh
 docker container prune -f
 #source ./includes/debug.sh
-docker-compose --project-name=$config_profile --env-file=profiles/$config_profile/.env up 
+docker-compose --project-name=$config_profile --env-file=profiles/$config_profile/.env up -d
 docker container prune -f
