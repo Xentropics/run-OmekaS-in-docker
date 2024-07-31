32d03da5c56a009a3cb11d2f238511427cd5690a
Jeroen Seeverens <j.seeverens@xentropics.nl>
2024-07-31 12:21:38 +0200

    removed profiles from repo

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@ source ./includes/defaults.sh
 source ./includes/getopts.sh
 #source ./includes/debug.sh
 docker build . --tag=xentropics/omeka-s:$OMEKA_IMAGE_TAG $par_omeka_s_version
-./run.sh "$@"
 
 
