bff365dc1ca173aa9a4314abc05f00573abc4152
Jeroen Seeverens <j.seeverens@xentropics.nl>
2024-07-31 12:57:57 +0200

    sync profiles from other repo before build

--- build_amd64.sh
+++ build_amd64.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source ./includes/defaults.sh
+source ./includes/profiles.sh
 source ./includes/getopts.sh
 #source ./includes/debug.sh
 docker build . --platform linux/amd64 --tag=xentropics/omeka-s:$OMEKA_IMAGE_TAG $par_omeka_s_version
