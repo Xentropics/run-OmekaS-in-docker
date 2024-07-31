a3dde5c890399a3e29761c346eef4a338cec61b5
Jeroen Seeverens <j.seeverens@xentropics.nl>
2024-07-31 16:43:46 +0200

    streamlined scripts added merged docker-compose.yml

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-source ./includes/defaults.sh
-source ./includes/getopts.sh
-docker container prune -f
-#source ./includes/debug.sh
-docker-compose --project-name=$config_profile --env-file=profiles/$config_profile/.env up -d
-docker container prune -f
+source ./includes/prime.sh
+docker-compose --project-name=$config_profile --env-file=profiles/$config_profile/.env -f docker-compose.yml -f profiles/$config_profile/docker-compose.yml up -d
