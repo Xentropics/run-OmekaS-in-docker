07c09b7ba81ba6d3f7eb96841072a31163582524
Jeroen Seeverens <j.seeverens@xentropics.nl>
2024-08-08 14:56:29 +0200

    bump

--- includes/defaults.sh
+++ includes/defaults.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 debug="${DEBUG:-no}"
 skip_modules="${SKIP_MODULES:-yes}"
-par_image_tag="${PAR_IMAGE_TAG:-0.6.5}"
+par_image_tag="${PAR_IMAGE_TAG:-0.6.6}"
 profile_dir="${PROFILE_DIR:-../xentropics-omeka-s-docker-profiles/profiles}"
 
