103355cc9110dcd42b8f5b1475ec7669717688b0
Jeroen Seeverens <j.seeverens@xentropics.nl>
2024-08-08 13:43:34 +0200

    dropping docker-compose,switching to docker compose

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source ./includes/prime.sh
-docker-compose --project-name=$config_profile --env-file=profiles/$config_profile/.env -f docker-compose.yml -f profiles/$config_profile/docker-compose.yml up -d
+docker compose --project-name=$config_profile --env-file=profiles/$config_profile/.env -f docker-compose.yml -f profiles/$config_profile/docker-compose.yml up -d
