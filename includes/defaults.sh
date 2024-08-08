0d5fffe3c72b6cfaf9d2c661ce09cb68631146e5
Jeroen Seeverens <j.seeverens@xentropics.nl>
2024-08-08 15:05:46 +0200

    .env shenanigans

--- includes/defaults.sh
+++ includes/defaults.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 debug="${DEBUG:-no}"
 skip_modules="${SKIP_MODULES:-yes}"
-par_image_tag="${PAR_IMAGE_TAG:-0.6.6}"
+par_image_tag="${PAR_IMAGE_TAG:-0.6.7}"
 profile_dir="${PROFILE_DIR:-../xentropics-omeka-s-docker-profiles/profiles}"
 
